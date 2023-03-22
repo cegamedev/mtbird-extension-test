896089d64bcaaef03eb9ff922640597bdd6deeab
cegamedev <cegamedev@gmail.com>
2023-03-22 14:52:03 +0800

    first commit

---
+++ src/features/Example/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const Example = () => {
+  return (
+    <div>
+      MTBIRD <Button>go home</Button>
+    </div>
+  );
+};
+
+export default Example;
